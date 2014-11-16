870d413b638596bc691248049ecbee1572ad62b8
headzoo <sean@headzoo.io>
2014-11-16 06:52:44 -0500

    Added GetLogger()

--- global.go
+++ global.go
@@ -2,202 +2,222 @@ package xlog
 
 import "io"
 
-// globalInstance stores the global logger.
-var globalInstance *Logger
+var (
+	// globalInstance stores the global logger.
+	globalInstance *Logger
 
-// globalAppended stores whether files have been appended to the global logger.
-var globalAppended bool = false
+	// globalAppended stores whether files have been appended to the global logger.
+	globalAppended bool = false
 
-// instance returns the global logger.
-func instance() *Logger {
+	// globalLoggers stores the loggers created by the GetLogger() function.
+	globalLoggers map[string]*Logger
+)
+
+// Instance returns the global logger.
+func Instance() *Logger {
 	if globalInstance == nil {
 		globalInstance = NewLogger("xlog")
 		globalInstance.Append("stdout", DebugLevel)
 		globalAppended = false
 	}
+	
 	return globalInstance
+}
+
+// GetLogger returns the *Logger with the given name. The logger will be
+// created if it's not already been created. Only a single *Logger instance
+// is created for a name.
+func GetLogger(name string) *Logger {
+	if globalLoggers == nil {
+		globalLoggers = make(map[string]*Logger)
+	}
+	if _, ok := globalLoggers[name]; !ok {
+		globalLoggers[name] = NewLogger(name)
+	}
+	
+	return globalLoggers[name]
 }
 
 // Close releases any resources held by the global logger. The logger should
 // not be used again after calling this method without re-configuring it, as
 // this method sets the global instance to nil.
 func Close() {
-	instance().Close()
+	Instance().Close()
 	globalInstance = nil
 }
 
 // SetName sets the name of the global logger.
 func SetName(name string) {
-	instance().SetName(name)
+	Instance().SetName(name)
 }
 
 // SetFormatter sets the formatter used by the global logger.
 func SetFormatter(formatter Formatter) {
-	instance().Formatter = formatter
+	Instance().Formatter = formatter
 }
 
 // SetLoggerMap sets the logger map used by the global logger.
 func SetLoggerMap(lm LoggerMap) {
-	instance().Loggers = lm
+	Instance().Loggers = lm
 }
 
 // Enabled returns whether the global logger is enabled.
 func Enabled() bool {
-	return instance().Enabled
+	return Instance().Enabled
 }
 
 // SetEnabled sets whether the global logger is enabled.
 func SetEnabled(enabled bool) {
-	instance().Enabled = enabled
+	Instance().Enabled = enabled
 }
 
 // Append adds a file to the global logger.
 func Append(file string, level Level) {
 	if !globalAppended {
-		instance().ClearAppended()
+		Instance().ClearAppended()
 		globalAppended = true
 	}
-	instance().Append(file, level)
+	Instance().Append(file, level)
 }
 
 // MultiAppend adds one or more files to the global logger.
 func MultiAppend(files []string, level Level) {
 	if !globalAppended {
-		instance().ClearAppended()
+		Instance().ClearAppended()
 		globalAppended = true
 	}
-	instance().MultiAppend(files, level)
+	Instance().MultiAppend(files, level)
 }
 
 // AppendWriter adds a writer to the global logger.
 func AppendWriter(writer io.Writer, level Level) {
 	if !globalAppended {
-		instance().ClearAppended()
+		Instance().ClearAppended()
 		globalAppended = true
 	}
-	instance().AppendWriter(writer, level)
+	Instance().AppendWriter(writer, level)
 }
 
 // MultiAppendWriters adds one or more io.Writer instances to the global logger.
 func MultiAppendWriters(writers []io.Writer, level Level) {
 	if !globalAppended {
-		instance().ClearAppended()
+		Instance().ClearAppended()
 		globalAppended = true
 	}
-	instance().MultiAppendWriters(writers, level)
+	Instance().MultiAppendWriters(writers, level)
 }
 
 // Writable returns true when global logging is enabled, and the global logger
 // hasn't been closed.
 func Writable() bool {
-	return instance().Writable()
+	return Instance().Writable()
 }
 
 // Log writes the message to each logger appended to the global logger at given level
 // or higher.
 func Log(level Level, v ...interface{}) {
-	instance().Log(level, v...)
+	Instance().Log(level, v...)
 }
 
 // Logf writes the message to each logger appended to the global logger given
 // level or higher.
 func Logf(level Level, format string, v ...interface{}) {
-	instance().Logf(level, format, v...)
+	Instance().Logf(level, format, v...)
 }
 
 // Debug writes to the global logger at DebugLevel.
 // Arguments are handled in the manner of fmt.Print.
 func Debug(v ...interface{}) {
-	instance().Debug(v...)
+	Instance().Debug(v...)
 }
 
 // Debugf writes to the global logger at DebugLevel.
 // Arguments are handled in the manner of fmt.Printf.
 func Debugf(format string, v ...interface{}) {
-	instance().Debugf(format, v...)
+	Instance().Debugf(format, v...)
 }
 
 // Info writes to the global logger at InfoLevel.
 // Arguments are handled in the manner of fmt.Print.
 func Info(v ...interface{}) {
-	instance().Info(v...)
+	Instance().Info(v...)
 }
 
 // Infof writes to the global logger at InfoLevel.
 // Arguments are handled in the manner of fmt.Printf.
 func Infof(format string, v ...interface{}) {
-	instance().Infof(format, v...)
+	Instance().Infof(format, v...)
 }
 
 // Notice writes to the global logger at NoticeLevel.
 // Arguments are handled in the manner of fmt.Print.
 func Notice(v ...interface{}) {
-	instance().Notice(v...)
+	Instance().Notice(v...)
 }
 
 // Noticef writes to the global logger at NoticeLevel.
 // Arguments are handled in the manner of fmt.Printf.
 func Noticef(format string, v ...interface{}) {
-	instance().Noticef(format, v...)
+	Instance().Noticef(format, v...)
 }
 
 // Warning writes to the global logger at WarningLevel.
 // Arguments are handled in the manner of fmt.Print.
 func Warning(v ...interface{}) {
-	instance().Warning(v...)
+	Instance().Warning(v...)
 }
 
 // Warningf writes to the global logger at WarningLevel.
 // Arguments are handled in the manner of fmt.Printf.
 func Warningf(format string, v ...interface{}) {
-	instance().Warningf(format, v...)
+	Instance().Warningf(format, v...)
 }
 
 // Error writes to the global logger at ErrorLevel.
 // Arguments are handled in the manner of fmt.Print.
 func Error(v ...interface{}) {
-	instance().Error(v...)
+	Instance().Error(v...)
 }
 
 // Errorf writes to the global logger at ErrorLevel.
 // Arguments are handled in the manner of fmt.Printf.
 func Errorf(format string, v ...interface{}) {
-	instance().Errorf(format, v...)
+	Instance().Errorf(format, v...)
 }
 
 // Critical writes to the global logger at CriticalLevel.
 // Arguments are handled in the manner of fmt.Print.
 func Critical(v ...interface{}) {
-	instance().Critical(v...)
+	Instance().Critical(v...)
 }
 
 // Criticalf writes to the global logger at CriticalLevel.
 // Arguments are handled in the manner of fmt.Printf.
 func Criticalf(format string, v ...interface{}) {
-	instance().Criticalf(format, v...)
+	Instance().Criticalf(format, v...)
 }
 
 // Alert writes to the global logger at AlertLevel.
 // Arguments are handled in the manner of fmt.Print.
 func Alert(v ...interface{}) {
-	instance().Alert(v...)
+	Instance().Alert(v...)
 }
 
 // Alertf writes to the global logger at AlertLevel.
 // Arguments are handled in the manner of fmt.Printf.
 func Alertf(format string, v ...interface{}) {
-	instance().Alertf(format, v...)
+	Instance().Alertf(format, v...)
 }
 
 // Emergency writes to the global logger at EmergencyLevel.
 // Arguments are handled in the manner of fmt.Print.
 func Emergency(v ...interface{}) {
-	instance().Emergency(v...)
+	Instance().Emergency(v...)
 }
 
 // Emergencyf writes to the global logger at EmergencyLevel.
 // Arguments are handled in the manner of fmt.Printf.
 func Emergencyf(format string, v ...interface{}) {
-	instance().Emergencyf(format, v...)
+	Instance().Emergencyf(format, v...)
 }
 
